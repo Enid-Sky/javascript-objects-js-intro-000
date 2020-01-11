f9dc7ae531db5a84fce89110ae740595c9459d16
enid svymbersky <enid.bersky@gmail.com>
2020-01-11 23:17:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {prince: 'purple rain'};
 
-function updatePlatlist (obj, artist, song) {
-  obj['artist'] = 'song';
+function updatePlaylist (playlist, artist, song) {
+  obj[artist] = 'song';
   return playlist;
 }
 
