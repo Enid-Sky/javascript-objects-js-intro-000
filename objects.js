e3f2b64c05d55890409b96e238022a9d3f1379e1
enid svymbersky <enid.bersky@gmail.com>
2020-01-11 23:20:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -5,6 +5,11 @@ function updatePlaylist (playlist, artist, song) {
   return playlist;
 }
 
+function removeFromPlaylist (playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
+
 
 
 /*
